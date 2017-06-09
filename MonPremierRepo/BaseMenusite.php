15a3ddae03179c007f4bc9ddf62d234eb64fe4a3
dotrakidovakin <p.desaintmeleuc@laposte.net>
2017-06-09 16:23:44 +0200

    Add files via upload

---
+++ MonPremierRepo/BaseMenusite.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<title>PAGE DU MENU</title>
+	<link href="css/bootstrap.css" rel="stylesheet">
+	</head>
+	
+	<body>
+	<?php  echo"ceci est du texte comptenant l'age du visiteur"
+	$age_du_visiteur = 20.5;
+	echo"l'age du visiteur est $age_du_visiteur";
+	?>
+
+
+
+	</body>
+	
+</html>
